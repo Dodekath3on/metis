c392157651016eeeaec9d7d19601b81dc9a3e11f
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2023-12-21 17:57:17 -0700

    added top navbar

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -6,6 +6,12 @@ import DialogTitle from '@mui/joy/DialogTitle';
 import ModalClose from '@mui/joy/ModalClose';
 import MenuIcon from '@mui/icons-material/Menu';
 import Divider from '@mui/joy/Divider';
+import AppBar from '@mui/material/AppBar'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Avatar from '@mui/material/Avatar'
 
 import List from '@mui/joy/List';
 
@@ -16,19 +22,51 @@ export default function DrawerCloseButton() {
   const [open, setOpen] = React.useState(false);
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Button variant="simple" color="neutral" onClick={() => setOpen(true)}>
-        <MenuIcon />
-      </Button>
-      <Drawer open={ open } onClose={() => setOpen(false)}>
-        <ModalClose />
-        <br/>
-        <List>
-          <NavSelection />
-          <Divider />
-          <NavProjects />
-        </List>
-      </Drawer>
-    </Box>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ display: 'flex' }}>
+            <Button variant="simple" color="neutral" onClick={() => setOpen(true)}>
+              <MenuIcon sx={{ color: 'white'}}/>
+            </Button>
+            <Drawer open={ open } onClose={() => setOpen(false)}>
+              <ModalClose />
+              <br/>
+              <List>
+                <NavSelection />
+                <Divider />
+                <NavProjects />
+              </List>
+            </Drawer>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            METIS
+          </Typography>
+          <Box sx={{ flexGrow: 1, textAlign: 'right' }}>
+            <IconButton>
+              <Avatar alt="Avatar" src="" />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+
+
+
+
   );
 }
