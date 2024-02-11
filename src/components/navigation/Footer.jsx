e02e35e023b0228857dd8d7c6c82ab7fb4e9714f
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2024-02-10 19:55:06 -0700

    updating contact page

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='flex bottom-0 px-8 py-8 w-full text-sm'>
-      {/* TODO: Update to use footerConsts */}
-      <ul>
-        <li>
+    <footer className='flex bg-secondary bottom-0 px-8 py-4 w-full text-sm'>
+      {/* TODO: Update to use footerConsts and change to icons */}
+      <ul className='flex'>
+        <li className='px-2'>
           <a href="#">Instagram</a>
         </li>
-        <li>
+        <li className='px-2'>
             <a href="#">X</a>
         </li>
-        <li>
+        <li className='px-2'>
             <a href="#">X</a>
         </li>
       </ul>
-      <p>copyright 2023</p>
+      <p>&copy;2024 Dodekath3on LLC</p>
     </footer>
   )
 }
